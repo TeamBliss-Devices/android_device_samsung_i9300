1d4063727f6e07cb6228063990a48d169a773e0b
Erwan Leboucher <erwanleboucher@gmail.com>
2015-07-13 18:54:52 +0200

    Update and rename cm.mk to bliss.mk

--- bliss.mk
+++ bliss.mk
@@ -6,17 +6,14 @@ TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/i9300/full_i9300.mk)
 
-# Custom unofficial build tag
-TARGET_UNOFFICIAL_BUILD_ID := temasek
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := i9300
-PRODUCT_NAME := cm_i9300
+PRODUCT_NAME := bliss_i9300
 PRODUCT_BRAND := samsung
 PRODUCT_MODEL := GT-I9300
 PRODUCT_MANUFACTURER := samsung
